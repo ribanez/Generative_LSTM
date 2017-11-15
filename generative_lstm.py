cb82c5afb40b9490918a5e93847aa161b184f3f6
robibanez <mail@ribanez.cl>
2017-11-15 14:34:52 -0300

    version 0.1

--- generative_lstm.py
+++ generative_lstm.py
@@ -1,8 +1,4 @@
-'''Example script to generate text from Nietzsche's writings.
-At least 20 epochs are required before the generated text
-starts sounding coherent.
-It is recommended to run this script on GPU, as recurrent
-networks are quite computationally intensive.
+'''Example script to generate text from diferent writings.
 If you try this script on new data, make sure your corpus
 has at least ~100k characters. ~1M is better.
 '''
@@ -19,22 +15,28 @@ import sys
 import matplotlib.pyplot as plt
 import os
 
+def clear_file(path_in, path_out):
+    ## Delete empty lines in original file
+    try:
+        os.path.isfile(path_out)
+        print("File clear exist")
+    except :
+        print("File clear don't exist")
+    else:
+        print("Creating File Clear")
+        with open(path_in) as infile, open(path_out, "w") as outfile:
+            for line in infile:
+                if not line.strip(): continue  # skip the empty line
+                outfile.write(line)  # non-empty line. Write it to output
+
 writter = 'nicanor'
 path_in = './Documents/' + writter + '.txt'
 path_out = './Documents/' + writter + '_clear.txt'
 
+## Delete empty lines in original file
+clear_file(path_in, path_out)
 
-## Delete empty lines 
-if os.path.isfile(path_out):
-    print('File Clear Exist')
-else:
-    print('Creating File Clear') 
-    with open(path_in) as infile, open(path_out, 'w') as outfile:
-        for line in infile:
-            if not line.strip(): continue  # skip the empty line
-            outfile.write(line)  # non-empty line. Write it to output
-
-
+## Read file
 path = path_out
 text = open(path).read().lower()
 print('corpus length:', len(text))
@@ -44,7 +46,7 @@ print('total chars:', len(chars))
 char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
-# cut the text in semi-redundant sequences of maxlen characters
+# Cut the text in semi-redundant sequences of maxlen characters
 maxlen = 40
 step = 3
 sentences = []
@@ -63,7 +65,7 @@ for i, sentence in enumerate(sentences):
     y[i, char_indices[next_chars[i]]] = 1
 
 
-# build the model: a single LSTM
+# Build the model: a single LSTM
 print('Build model...')
 model = Sequential()
 model.add(LSTM(128, input_shape=(maxlen, len(chars))))
@@ -83,44 +85,46 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
-# train the model, output generated text after each iteration
-for iteration in range(1, 5000):
+# train the model
+batch_size = 128
+n_epochs = 1000
+
+print()
+print('-' * 50)
+historial = model.fit(x, y,
+          batch_size=batch_size,
+          epochs=n_epochs)
+
+plt.plot(historial.history["loss"])
+plt.show()
+
+start_index = random.randint(0, len(text) - maxlen - 1)
+
+## Generate text with different diversity
+for diversity in [0.2, 0.5, 1.0, 1.2]:
     print()
-    print('-' * 50)
-    print('Iteration', iteration)
-    historial = model.fit(x, y,
-              batch_size=128,
-              epochs=2)
-    
-    #plt.plot(historial.history["loss"])
-    #plt.show()
+    print('----- diversity:', diversity)
 
-    start_index = random.randint(0, len(text) - maxlen - 1)
+    generated = ''
+    sentence = text[start_index: start_index + maxlen]
+    generated += sentence
+    print('----- Generating with seed: "' + sentence + '"')
+    sys.stdout.write(generated)
 
-    for diversity in [0.2, 0.5, 1.0, 1.2]:
-        print()
-        print('----- diversity:', diversity)
+    for i in range(255):
+        x_pred = np.zeros((1, maxlen, len(chars)))
+        for t, char in enumerate(sentence):
+            x_pred[0, t, char_indices[char]] = 1.
 
-        generated = ''
-        sentence = text[start_index: start_index + maxlen]
-        generated += sentence
-        print('----- Generating with seed: "' + sentence + '"')
-        sys.stdout.write(generated)
+        preds = model.predict(x_pred, verbose=0)[0]
+        next_index = sample(preds, diversity)
+        next_char = indices_char[next_index]
 
-        for i in range(400):
-            x_pred = np.zeros((1, maxlen, len(chars)))
-            for t, char in enumerate(sentence):
-                x_pred[0, t, char_indices[char]] = 1.
+        generated += next_char
+        sentence = sentence[1:] + next_char
 
-            preds = model.predict(x_pred, verbose=0)[0]
-            next_index = sample(preds, diversity)
-            next_char = indices_char[next_index]
-
-            generated += next_char
-            sentence = sentence[1:] + next_char
-
-            sys.stdout.write(next_char)
-            sys.stdout.flush()
+        sys.stdout.write(next_char)
+        sys.stdout.flush()
 
 print("-"*50)
 print("---- Save Weights Model ----")
